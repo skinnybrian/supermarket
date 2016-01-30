7ba89a7a5aba91b958ef644e8d6df0914fe1c65b
Shinnosuke HORIGUCHI <post@skinnybrian.com>
2016-01-30 16:04:32 +0900

    add item_database_spec

---
+++ spec/item_database_spec.rb
@@ -0,0 +1,17 @@
+require 'item'
+require 'item_database'
+
+RSpec.describe ItemDetabase do
+    it do
+        apple = Item.new('Apple', 100)
+        banana = Item.new('Banana', 50)
+
+        db = ItemDetabase.new
+        db.add(apple)
+        db.add(banana)
+
+        expect(db.find('Apple')).to eq(apple)
+        expect(db.find('Banana')).to eq(banana)
+    end
+end
+        
