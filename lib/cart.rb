5b531a530d67adb7f4293fcad1e20b486b3c4466
Shinnosuke HORIGUCHI <post@skinnybrian.com>
2016-01-30 15:29:21 +0900

    fix cart.rb,cart_spec.rb

--- lib/cart.rb
+++ lib/cart.rb
@@ -1,8 +1,21 @@
 class Cart
-    attr_accessor :cart, :total
 
-    def initialize(name, price)
-        @cart = name
-        @total = price
+    def initialize
+        @total = 0
+        @items = []
     end
+
+    def add(item)
+        @total += item.price
+        @items.push(item)
+    end
+
+    def total
+        @total
+    end
+
+    def items
+        @items
+    end
+
 end
